177416c4f2d17bd61d7182f68d73b950e431a46b
arpita <arpitaagrahari004@gmail.com>
2024-10-01 23:18:30 +0530

    Refactor Sticky Notes implementation in script.js

--- projects/Sticky-Note/script.js
+++ projects/Sticky-Note/script.js
@@ -3,10 +3,10 @@ document.getElementById('add-note').addEventListener('click', addNote);
 function addNote() {
     const note = document.createElement('div');
     note.classList.add('note');
-
+    //textarea implementation
     const textarea = document.createElement('textarea');
     textarea.placeholder = "Write your note here...";
-
+    //edit button implementation
     const editButton = document.createElement('button');
     editButton.textContent = 'Edit';
     editButton.classList.add('edit-note');
@@ -20,13 +20,14 @@ function addNote() {
         }
     });
 
+    //delete button implementation
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'X';
     deleteButton.classList.add('delete-note');
     deleteButton.addEventListener('click', () => {
         note.remove(); 
     });
-
+    //edit and appending 
     note.appendChild(textarea);
     note.appendChild(editButton);
     note.appendChild(deleteButton);
