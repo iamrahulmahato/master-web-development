a39bdf3533d644147129a6fbd1c8920fa528a0a3
souvikpramanikgit <souvikpramanik874@gmail.com>
2024-10-05 22:04:04 +0530

    Add Notes taking

---
+++ projects/Notes Taking Website/js/NoteApi.js
@@ -0,0 +1,34 @@
+export default class NotesAPI {
+    static getAllNotes() {
+        const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
+
+        return notes.sort((a, b) => {
+            return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
+        });
+    }
+
+    static saveNote(noteToSave) {
+        const notes = NotesAPI.getAllNotes();
+        const existing = notes.find(note => note.id == noteToSave.id);
+
+        // Edit/Update
+        if (existing) {
+            existing.title = noteToSave.title;
+            existing.body = noteToSave.body;
+            existing.updated = new Date().toISOString();
+        } else {
+            noteToSave.id = Math.floor(Math.random() * 1000000);
+            noteToSave.updated = new Date().toISOString();
+            notes.push(noteToSave);
+        }
+
+        localStorage.setItem("notesapp-notes", JSON.stringify(notes));
+    }
+
+    static deleteNote(id) {
+        const notes = NotesAPI.getAllNotes();
+        const newNotes = notes.filter(note => note.id != id);
+
+        localStorage.setItem("notesapp-notes", JSON.stringify(newNotes));
+    }
+}
