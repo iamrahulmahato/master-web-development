00fe64e90dd75d8185257a3f66fcc90c5a196928
Adarsh Gupta <153114467+adarshgupta14@users.noreply.github.com>
2024-10-05 14:31:52 +0530

    javascript.js

--- projects/bmi calculator/javascript.js
+++ projects/bmi calculator/javascript.js
@@ -7,11 +7,11 @@ window.onload = function () {
         let weight = document.getElementById("weight").value;
         let height = document.getElementById("height").value;
 
-        if (height === '' || height < 0 || isNaN(height)) {
-            document.getElementById("result-message").innerHTML = `Enter a valid height ${height}`;
+        if (weight === '' || weight < 0 || isNaN(weight)) {
+            document.getElementById("result-message").innerHTML = `Enter a valid weight`;
         }
-        else if (weight === '' || weight < 0 || isNaN(weight)) {
-            document.getElementById("result-message").innerHTML = `Enter a valid weight ${weight}`;
+        else if (height === '' || height < 0 || isNaN(height)) {
+            document.getElementById("result-message").innerHTML = `Enter a valid height`;
         }
         else {
                 const bmi = (weight / ((height * height) / 10000)).toFixed(2)
