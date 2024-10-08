6bbe23dc1f814c133b4481b8c2382c9bb497d1fc
Titus210 <tituskiplagat210@gmail.com>
2024-10-08 22:38:06 +0300

    Sample data for images and text

--- projects/mozilla-text-to-speech/script.js
+++ projects/mozilla-text-to-speech/script.js
@@ -5,3 +5,19 @@ const textarea = document.getElementById('text');
 const readBtn = document.getElementById('read');
 const toggleBtn = document.getElementById('toggle');
 const closeBtn = document.getElementById('close');
+
+// Sample data for images and text
+const data = [
+    { image: './img/drink.jpg', text: "Get me some water please. I'm thirsty." },
+    { image: './img/food.jpg', text: "I am hungry. Get me some food." },
+    { image: './img/tired.jpg', text: "I am tired. I need to sleep." },
+    { image: './img/hurt.jpg', text: "I am hurt. I need a doctor." },
+    { image: './img/happy.jpg', text: "I am happy. I am feeling good." },
+    { image: './img/angry.jpg', text: "I am angry. I am feeling bad." },
+    { image: './img/sad.jpg', text: "I am sad. I am feeling down." },
+    { image: './img/scared.jpg', text: "I am scared. I am feeling uneasy." },
+    { image: './img/outside.jpg', text: "I want to go outside. I want to play." },
+    { image: './img/home.jpg', text: "I want to go home. I want to rest." },
+    { image: './img/school.jpg', text: "I want to go to school. I want to study." },
+    { image: './img/grandma.jpg', text: "I want to go to grandma's house. I want to visit." }
+];
