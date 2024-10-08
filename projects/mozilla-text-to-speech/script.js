962412b5d885d21622833d81cc7a8016b06645c4
Titus210 <tituskiplagat210@gmail.com>
2024-10-08 22:37:45 +0300

    Get DOM elements

---
+++ projects/mozilla-text-to-speech/script.js
@@ -0,0 +1,7 @@
+// Get DOM elements
+const main = document.querySelector('main');
+const voiceSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
