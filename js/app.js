a251c602c72027f30264be6fd914a3ffa78db9da
Anurag Bansal <devanuragbansal@gmail.com>
2024-10-17 19:28:14 +0530

    theme toggling fixed

--- js/app.js
+++ js/app.js
@@ -4,16 +4,16 @@ const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 let nextBtn = document.querySelector('.next')
 let prevBtn = document.querySelector('.prev')
 
-/*slider section*/
+/* slider section */
 let slider = document.querySelector('.slider')
 let sliderList = slider.querySelector('.slider .list')
 let thumbnail = document.querySelector('.slider .thumbnail')
 let thumbnailItems = thumbnail.querySelectorAll('.item')
-let slideButton = document.querySelector('.button button ')
+let slideButton = document.querySelector('.button button')
 thumbnail.appendChild(thumbnailItems[0])
 
 slideButton.onclick = function() {
-  moveSlider('next')
+    moveSlider('next')
 }
 
 // Function for next button 
@@ -30,7 +30,7 @@ function moveSlider(direction) {
     let sliderItems = sliderList.querySelectorAll('.item')
     let thumbnailItems = document.querySelectorAll('.thumbnail .item')
     
-    if(direction === 'next'){
+    if (direction === 'next') {
         sliderList.appendChild(sliderItems[0])
         thumbnail.appendChild(thumbnailItems[0])
         slider.classList.add('next')
@@ -41,35 +41,40 @@ function moveSlider(direction) {
     }
 
     slider.addEventListener('animationend', function() {
-        if(direction === 'next'){
+        if (direction === 'next') {
             slider.classList.remove('next')
         } else {
             slider.classList.remove('prev')
         }
     }, { once: true }) // Remove the event listener after it's triggered once
 }
-/*slider section*/
+/* slider section */
 
-if (prefersDarkScheme.matches) {
-  document.documentElement.setAttribute("data-theme", "dark");
+// Check localStorage for theme preference
+const storedTheme = localStorage.getItem("theme")
+if (storedTheme) {
+    document.documentElement.setAttribute("data-theme", storedTheme)
+} else if (prefersDarkScheme.matches) {
+    document.documentElement.setAttribute("data-theme", "dark")
 } else {
-  document.documentElement.setAttribute("data-theme", "light");
+    document.documentElement.setAttribute("data-theme", "light")
 }
 
 toggleModeBtn.onclick = () => {
-    if (document.documentElement.getAttribute("data-theme") === "light") {
-        document.documentElement.setAttribute("data-theme", "dark");
-        // toggleModeBtn.innerText = "Light";
-        toggleModeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 20 20" fill="currentColor">
-        <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />
-      </svg>`;
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme); // Store the preference in localStorage
 
+    if (newTheme === "dark") {
+        toggleModeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />
+        </svg>`;
     } else {
-        document.documentElement.setAttribute("data-theme", "light");
-        // toggleModeBtn.innerText = "Dark";
         toggleModeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 20 20" fill="#1e293b">
-        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-      </svg>`;
+            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+        </svg>`;
     }
 }
 
@@ -81,8 +86,7 @@ window.onscroll = function() {
 };
 
 function scrollFunction() {
-
-    if ( document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         scrollToTopBtn.style.display = "block"; 
         scrollToTopBtn.style.opacity = "1"; 
     } else {
@@ -91,7 +95,6 @@ function scrollFunction() {
         setTimeout(() => {
             scrollToTopBtn.style.display = "none";
         }, 500); 
-
     }
 }
 
@@ -110,7 +113,6 @@ function filterCards() {
     // Loop through the cards and filter based on the input
     cards.forEach(function (card) {
         let cardHeading = card.querySelector('.card-heading').innerText.toLowerCase();
-        
         let cardDescription = card.querySelector('.card-description').innerText.toLowerCase();
         
         // Check if the search term is in the card heading or description
@@ -128,66 +130,65 @@ document.getElementById('search-input').addEventListener('input', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  const cardContainer = document.getElementsByClassName('projects-container')[0];
-  const cards = Array.from(cardContainer.getElementsByClassName('card'));
-  console.log(cards);
-
-  // Sort cards alphabetically based on the card title
-  cards.sort((a, b) => {
-    const headingA = a.querySelector('.card-heading');
-    const headingB = b.querySelector('.card-heading');
+    const cardContainer = document.getElementsByClassName('projects-container')[0];
+    const cards = Array.from(cardContainer.getElementsByClassName('card'));
     
-    // Check if both headings exist before sorting
-    if (headingA && headingB) {
-        const titleA = headingA.innerText.toUpperCase();
-        const titleB = headingB.innerText.toUpperCase();
-        return titleA.localeCompare(titleB);
-    } else {
-        // Handle cases where card headings are missing
-        console.warn('One or both cards are missing headings.');
-        return 0;
-    }
-});
-  // Remove current cards and append them in the new order
-  cardContainer.innerHTML = '';
-  cards.forEach(card => {
-      cardContainer.appendChild(card);
-  });
+    // Sort cards alphabetically based on the card title
+    cards.sort((a, b) => {
+        const headingA = a.querySelector('.card-heading');
+        const headingB = b.querySelector('.card-heading');
+        
+        // Check if both headings exist before sorting
+        if (headingA && headingB) {
+            const titleA = headingA.innerText.toUpperCase();
+            const titleB = headingB.innerText.toUpperCase();
+            return titleA.localeCompare(titleB);
+        } else {
+            // Handle cases where card headings are missing
+            console.warn('One or both cards are missing headings.');
+            return 0;
+        }
+    });
+    // Remove current cards and append them in the new order
+    cardContainer.innerHTML = '';
+    cards.forEach(card => {
+        cardContainer.appendChild(card);
+    });
 }, { once: true });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const coords = { x: 0, y: 0 };
-  const circles = document.querySelectorAll(".circle");
+    const coords = { x: 0, y: 0 };
+    const circles = document.querySelectorAll(".circle");
 
-  circles.forEach(function (circle) {
-    circle.x = 0;
-    circle.y = 0;
-  });
-
-  window.addEventListener("mousemove", function (e) {
-    coords.x = e.pageX;
-    coords.y = e.pageY - window.scrollY; // Adjust for vertical scroll position
-  });
-
-  function animateCircles() {
-    let x = coords.x;
-    let y = coords.y;
-
-    circles.forEach(function (circle, index) {
-      circle.style.left = `${x - 12}px`;
-      circle.style.top = `${y - 12}px`;
-      circle.style.transform = `scale(${(circles.length - index) / circles.length})`;
-
-      const nextCircle = circles[index + 1] || circles[0];
-      circle.x = x;
-      circle.y = y;
-
-      x += (nextCircle.x - x) * 0.3;
-      y += (nextCircle.y - y) * 0.3;
+    circles.forEach(function (circle) {
+        circle.x = 0;
+        circle.y = 0;
     });
 
-    requestAnimationFrame(animateCircles);
-  }
+    window.addEventListener("mousemove", function (e) {
+        coords.x = e.pageX;
+        coords.y = e.pageY - window.scrollY; // Adjust for vertical scroll position
+    });
 
-  animateCircles();
+    function animateCircles() {
+        let x = coords.x;
+        let y = coords.y;
+
+        circles.forEach(function (circle, index) {
+            circle.style.left = `${x - 12}px`;
+            circle.style.top = `${y - 12}px`;
+            circle.style.transform = `scale(${(circles.length - index) / circles.length})`;
+
+            const nextCircle = circles[index + 1] || circles[0];
+            circle.x = x;
+            circle.y = y;
+
+            x += (nextCircle.x - x) * 0.3;
+            y += (nextCircle.y - y) * 0.3;
+        });
+
+        requestAnimationFrame(animateCircles);
+    }
+
+    animateCircles();
 });
