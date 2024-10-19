ef82538ead6d5d417c6a93634272baaf4b7d3d3c
toshan kanwar <162002505+toshankanwar@users.noreply.github.com>
2024-10-19 11:05:59 +0530

    Add files via upload

---
+++ projects/Text-Analyzer/src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+export default function Navbar() {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/text-analyzer"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/text-analyzer/about"
+                >
+                  about
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/text-analyzer/contactus"
+                >
+                  contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
